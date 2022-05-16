6b3b4dc68e45f4506cbc1d8f77d3a009ec2b9e01
Xablau <102757880+iTzXablau123@users.noreply.github.com>
2022-05-16 14:36:43 -0300

    Add files via upload

---
+++ At02.js
@@ -0,0 +1,8 @@
+/*2. Faça um programa que exiba os números de 50 a 100*/
+
+let k = 50
+
+while (k <= 100){
+    console.log(c);
+     k = k + 1
+}
---
+++ At03.js
@@ -0,0 +1,9 @@
+/* 3_ Faça um programa que escreva a contagem regressiva do lançamento de um foguete. O
+programa deve imprimir 10,9, 8, ...,1,0.*/
+
+let k = 10
+
+while (k >= 0){
+    console.log(c);
+     k = k - 1
+}
